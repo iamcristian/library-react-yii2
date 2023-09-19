230b09e433c5af7dca237668b385a3b27e371ee2
cristian_arando <201902469@est.umss.edu>
2023-09-19 10:37:15 -0400

    feat: :art: fix the migrations and seeding database
    php yii migrate/create seed_member_table and php yii migrate/create seed_book_table

--- backend/migrations/m230919_133922_create_loan_table.php
+++ backend/migrations/m230919_133922_create_loan_table.php
@@ -16,16 +16,19 @@ class m230919_133922_create_loan_table extends Migration
             'id' => $this->primaryKey(),
             'book_id' => $this->integer(),
             'borrower_id' => $this->integer(),
-            'borrowed_on' => $this->dateTime()->defaultValue(date('Y-m-d H:i:s')),
+            'borrowed_on' => $this->dateTime()
+                ->defaultValue(date('Y-m-d H:i:s')),
             'to_be_returned_on' => $this->dateTime()
         ]);
 
+        // create index for column `book_id`
         $this->createIndex(
             'idx-loan-book_id',
             'loan',
             'book_id'
         );
-        
+
+        // add foreign key for table `post`
         $this->addForeignKey(
             'fk-loan-book_id',
             'loan',
@@ -35,8 +38,16 @@ class m230919_133922_create_loan_table extends Migration
             'CASCADE'
         );
 
+        // create index for column `borrower_id`
         $this->createIndex(
             'idx-loan-borrower_id',
+            'loan',
+            'borrower_id'
+        );
+
+        // add foreign key for table `post`
+        $this->addForeignKey(
+            'fk-loan-borrower_id',
             'loan',
             'borrower_id',
             'member',
@@ -50,7 +61,7 @@ class m230919_133922_create_loan_table extends Migration
      */
     public function safeDown()
     {
-        $this->dropTable('fk-loan-borrower_id', 'loan');
+        $this->dropForeignKey('fk-loan-borrower_id', 'loan');
         $this->dropIndex('idx-loan-borrower_id', 'loan');
         $this->dropForeignKey('fk-loan-book_id', 'loan');
         $this->dropIndex('idx-loan-book_id', 'loan');
