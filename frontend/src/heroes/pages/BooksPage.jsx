b6c118f287250312bd370aa1ed215d3fd6d5778a
cristian_arando <201902469@est.umss.edu>
2023-09-21 11:54:04 -0400

    start

--- frontend/src/heroes/pages/BooksPage.jsx
+++ frontend/src/heroes/pages/BooksPage.jsx
@@ -1,12 +1,19 @@
-import { BooksList } from "../components/BooksList"
+import { BooksList } from "../components/BooksList";
+
+let createBook = () => {
+  console.log("createBook");
+};
 
 export const BooksPage = () => {
   return (
     <>
-    <h1>Books</h1>
-    <hr />
+      <h1>Books</h1>
+      <button className="btn btn-primary" onClick={createBook}>
+        Add Book
+      </button>
+      <hr />
 
-      <BooksList/>
+      <BooksList />
     </>
-  )
-}
+  );
+};
